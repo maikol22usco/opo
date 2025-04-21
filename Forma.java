36d05d643e651f9a03d81ab40ff7947ff0cb6af5
maikol22usco <u20242228971@usco.edu.co>
2025-04-21 12:13:17 -0500

    Update Forma.java

---
+++ Forma.java
@@ -0,0 +1,48 @@
+public abstract class Forma {
+    protected String nombre;
+    protected double posicionX;
+    protected double posicionY;
+    protected String color;
+
+    public Forma(String nombre, double posicionX, double posicionY, String color) {
+        this.nombre = nombre;
+        this.posicionX = posicionX;
+        this.posicionY = posicionY;
+        this.color = color;
+    }
+
+    public abstract double calcularArea();
+
+    // Getters and setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPosicionX() {
+        return posicionX;
+    }
+
+    public void setPosicionX(double posicionX) {
+        this.posicionX = posicionX;
+    }
+
+    public double getPosicionY() {
+        return posicionY;
+    }
+
+    public void setPosicionY(double posicionY) {
+        this.posicionY = posicionY;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+}
